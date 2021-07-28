70f3d4f7cf1492e96b2f2b894c6a23d21ab1cdc3
Alex1989-star <bio181@yandex.ru>
2021-07-28 14:17:39 +0300

    initial commit

---
+++ TrafficLightsSwiftUI/ColorSignal.swift
@@ -0,0 +1,46 @@
+//
+//  ColorSignal.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Леся on 28.07.2021.
+//
+
+import SwiftUI
+
+struct ColorSignal: View {
+    var color: TrafficLightColor = .red
+    var state: TrafficLightState = .off
+    var lineWidth: CGFloat = 2
+    
+    private let circleSize: CGFloat = 95
+    
+    var body: some View {
+        switch color {
+        case .red:
+            signalCircle(color: .red, opacity: state.rawValue, circleSize: circleSize)
+        case .yellow:
+            signalCircle(color: .yellow, opacity: state.rawValue, circleSize: circleSize)
+        case .green:
+            signalCircle(color: .green, opacity: state.rawValue, circleSize: circleSize)
+        }
+    }
+    
+    private func signalCircle(color: Color, opacity: Double, circleSize: CGFloat) -> some View {
+        
+        Circle()
+            .stroke(color.opacity(0.3), lineWidth: lineWidth)
+            .background(
+                Circle().fill(RadialGradient(gradient: Gradient(colors: [.white, color]), center: .center, startRadius: 2, endRadius: 100))
+                    .opacity(opacity))
+            .frame(width: circleSize, height: circleSize)
+            .shadow(color: color, radius: 36)
+            .padding(.bottom, 24)
+    }
+}
+
+struct Signal_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorSignal().previewLayout(.sizeThatFits)
+    }
+}
+
--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -7,10 +7,62 @@
 
 import SwiftUI
 
+enum TrafficLightColor {
+    case red, yellow, green
+}
+
+enum TrafficLightState: Double {
+    case on = 1.0
+    case off = 0.2
+}
+
 struct ContentView: View {
+    @State private var started = false
+    @State var currentLight: TrafficLightColor?
+    
+    private var signals: [TrafficLightColor] = [.red, .yellow, .green]
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            //Color.white.ignoresSafeArea()
+            Image("road").ignoresSafeArea()
+            Image("trafficLight").frame(width: 170, height: 170, alignment: .bottom)
+            
+            VStack {
+                Spacer()
+                
+                ForEach(signals, id: \.self) { signalColor in
+                    ColorSignal(color: signalColor, state: currentLight == signalColor ? .on : .off)
+                }
+                
+                Spacer()
+                
+                VStack {
+                    Button(action:  {
+                        started = true
+                        
+                        switch currentLight {
+                        case .red:
+                            self.currentLight = .yellow
+                        case .yellow:
+                            self.currentLight = .green
+                        case .green, .none:
+                            self.currentLight = .red
+                        }
+                        
+                    }, label: {
+                        Text(started ? "Next" : "Start")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .padding(.all, 30)
+                            .frame(width: 200)
+                            .overlay(RoundedRectangle(cornerRadius: 25)
+                                        .stroke(Color.black, lineWidth: 3))
+                            .background(Color.purple.cornerRadius(25))
+                    }).padding(.bottom, 100)
+                }
+            }
+        }
     }
 }
 
